a7db5c6757ad0c6e8118a33e938acdaadf82a9d3
Roy_Naor <133534625+RoyNaor@users.noreply.github.com>
2025-12-01 22:05:58 +0200

    uploaded the lang changer

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,3 +1,4 @@
+import './src/i18n';
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
