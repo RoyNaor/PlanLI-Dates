1ee527d4d5c851774e71e2035d4317fcdc9dc339
Roy_Naor <133534625+RoyNaor@users.noreply.github.com>
2026-01-13 20:26:20 +0200

    Update firebase.ts

--- mobile-app/src/config/firebase.ts
+++ mobile-app/src/config/firebase.ts
@@ -5,12 +5,7 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZtLkKvpUPOUgEE7SqrK1GpxL5QddpaiU",
-  authDomain: "planli-dates-dev.firebaseapp.com",
-  projectId: "planli-dates-dev",
-  storageBucket: "planli-dates-dev.firebasestorage.app",
-  messagingSenderId: "406568693824",
-  appId: "1:406568693824:web:2260c9c6509cdf92b16fa5"
+  
 };
 
 const app = initializeApp(firebaseConfig);
